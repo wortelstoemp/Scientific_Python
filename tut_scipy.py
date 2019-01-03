8f9597160d578e44b50c5fa88a6bcf943b26cfd8
Tom Quareme <tquareme@in.edm.uhasselt.be>
2019-01-03 15:55:45 +0100

    Scipy

--- tut_scipy.py
+++ tut_scipy.py
@@ -55,3 +55,136 @@ np.allclose(a_reconstruction, a)
 
 # Other decompositions (QR, LU, Cholesky, Schur)
 # Linear solvers
+# TODO: https://docs.scipy.org/doc/scipy/reference/linalg.html#module-scipy.linalg
+
+# ------------------------------------------------------------------------------
+# Interpolation
+# ------------------------------------------------------------------------------
+# TODO: http://www.scipy-lectures.org/intro/scipy.html#interpolation-scipy-interpolate
+
+# Faked experimental data
+times = np.linspace(0, 1, 10)   # start, end (inclusive), num float points
+noise = (np.random.random(10)*2 - 1) * 1e-1
+measurements = np.sin(2 * np.pi * times) + noise
+
+# Interpolation (interp1d for 1D arrays, or interp2d for 2D arrays)
+from scipy.interpolate import interp1d
+linear_interp = interp1d(times, measurements)
+cubic_interp = interp1d(times, measurements, kind='cubic')
+
+# Evaluate interpolation at times of interest
+linear_interp = interp1d(times, measurements)
+interpolation_times = np.linspace(0, 1, 50)
+results = linear_interp(interpolation_times)
+
+# ------------------------------------------------------------------------------
+# Optimization
+# ------------------------------------------------------------------------------
+# TODO: http://www.scipy-lectures.org/advanced/mathematical_optimization/index.html#mathematical-optimization
+from scipy import optimize
+
+# Curve fitting (find parameters)
+def test_func(x, a, b):
+    return a * np.sin(b * x)    # a and b are params (amplitude and period)!!!
+
+x_data = np.linspace(-5, 5, num=50)
+y_data = 2.9 * np.sin(1.5 * x_data) + np.random.normal(size=50)
+
+params, params_covariance = optimize.curve_fit(test_func, x_data, y_data, p0=[2, 2])
+
+
+# Local minimum of function
+# minimize() works in general with x multidimensionsal!!!
+# If 1D function, you can just use scipy.optimize.minimize_scalar()
+def f(x):
+    return x**2 + 10*np.sin(x)
+
+starting_point = 0
+result = optimize.minimize(f, x0 = starting_point, method="L-BFGS-B")
+print(result.x)
+
+# Global minimum
+#   Basin-hopping is a two-phase method that combines a global 
+#   stepping algorithm with local minimization at each step.
+#   Other algorithms: shgo, dual_annealing, brute (= bad...)
+starting_point = 0
+result = optimize.basinhopping(f, x0 = starting_point, method="L-BFGS-B")
+print(result.x)
+
+# Constraints (vector variable x in []-interval)
+result = optimize.minimize(f, x0 = 1, bounds = ((0, 10), ))
+
+
+# Finding roots of function
+root = optimize.root(f, x0 = 1)
+print(root.x)
+
+# ------------------------------------------------------------------------------
+# Statistics
+# ------------------------------------------------------------------------------
+# TODO: http://www.scipy-lectures.org/packages/statistics/index.html#statistics 
+# Mean
+samples = np.random.normal(size = 1000)
+np.mean(samples)
+
+# Median (insensitive to outliers)
+samples = np.random.normal(size = 1000)
+np.median(samples) 
+
+# Percentiles (or CDF)
+# p'th percentile is the point where the CDF reaches p/100
+stats.scoreatpercentile(samples, 90)
+
+# Histogram and PDF's
+# TODO: http://www.scipy-lectures.org/intro/scipy.html#distributions-histogram-and-probability-density-function
+
+# T-Test test if means of 2 sets of observations (distributions) are close
+# T value: difference between the processes
+# p value: probability of 2 processes being equal
+a = np.random.normal(0, 1, size = 100)
+b = np.random.normal(1, 1, size = 10)
+stats.ttest_ind(a, b) # gives T, p values
+
+
+# ------------------------------------------------------------------------------
+# Numerical integration
+# ------------------------------------------------------------------------------
+from scipy.integrate import quad
+
+def f(x):
+    return np.sin(x)
+
+a = 0
+b = np.pi/2
+result, error_estimate = quad(f, a, b)
+
+# Other schemes: scipy.integrate.fixed_quad(), 
+# scipy.integrate.quadrature(), scipy.integrate.romberg()
+
+# ------------------------------------------------------------------------------
+# Integrating first-order ODE's (Ordinary Differential Equations)
+# of form: dy/dt = func(y, t, ...)  [or func(t, y, ...)]
+# ------------------------------------------------------------------------------
+
+def func(y, t):
+    return -2 * y
+
+from scipy.integrate import odeint
+
+times = np.linspace(0, 4, 40)
+y = odeint(func, y0=1, t=times)
+
+# ------------------------------------------------------------------------------
+# Fast Fourier Transforms (use SciPy and NOT Numpy)
+# ------------------------------------------------------------------------------
+# from scipy import fftpack
+# fft_signal = fftpack.fft(sig)
+# frequencies = fftpack.fftfreq(signal.size, d=time_step) 
+
+# ifft_signal = scipy.fftpack.ifft(fft_signal)
+
+
+# ------------------------------------------------------------------------------
+# Signal processing
+# ------------------------------------------------------------------------------
+# TODO: http://www.scipy-lectures.org/intro/scipy.html#signal-processing-scipy-signal
